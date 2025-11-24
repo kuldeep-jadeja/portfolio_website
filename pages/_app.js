94f599807b0ad7087d0e4ba68e568a4239a7b891
kuldeep-jadeja <47859382+kuldeep-jadeja@users.noreply.github.com>
2025-11-24 10:06:48 +0530

    Add Google Analytics integration to the app
    Add Google Analytics integration to the app

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,22 @@
 import "../styles/globals.scss";
 import { ThemeProvider } from '../contexts/ThemeContext';
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider>
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-H9RNHSBV4Y" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-H9RNHSBV4Y');
+        `}
+      </Script>
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
   );
 }
